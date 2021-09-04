356e43100104cf9d4bb7d67aad9208e80e0627e2
babakoto <e.tokiniaina@gmail.com>
2021-09-04 12:22:05 +0200

    update documentation swagger

--- app/src/api/api.controller.ts
+++ app/src/api/api.controller.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { HttpModule, Param, Query } from '@nestjs/common';
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { query } from 'express';
 import { firstValueFrom, Observable } from 'rxjs';
 import { ApiService } from './api.service';
@@ -10,6 +11,7 @@ export class ApiController {
     constructor(private apiService:ApiService){}
 
     @Get()
+    @ApiOperation({summary:"If you don't add lat & lon , Api will use lat & lon default value "})
     async getData(@Query("lat") lat:number, @Query("lon") lon:number){
         return this.apiService.getData(lat,lon);
     }
--- app/src/crons/crons.controller.ts
+++ app/src/crons/crons.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { CronsService } from './crons.service';
 
 @Controller('crons')
@@ -7,10 +8,12 @@ export class CronsController {
     constructor(private cronsService:CronsService){}
 
     @Get("/run")
+    @ApiOperation({ summary: 'the cron is launched when the application start , but you can use this after stopping' })
     async runCron(){
        return this.cronsService.runCron();
     }
 
+    @ApiOperation({ summary: 'when you want to stop the cron' })
     @Get("/stop")
     async stopCron(){
        return this.cronsService.stopCronParis();
